0cb2b293dbbd134808d6a74e2fcee0569c2f819e
jussimar <jussimar@jussimarleal.com.br>
2022-08-13 01:33:06 +0000

    criando layout basico

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo! </Text>
-      <StatusBar style="auto" />
+		<View style={styles.bloco}>
+	  		<Text style={styles.titulo}>Calculadora Simples </Text>
+		</View>
+		<View style={styles.bloco}>
+			<Text> Valor 1: </Text>
+			<TextInput style={styles.input}/>
+		</View>
+		<View style={styles.bloco}>
+			<Text> Valor 2: </Text>
+			<TextInput style={styles.input}/>
+		</View>
+		<View style={styles.bloco}>
+			<TouchableOpacity style={styles.botao}>
+				<Text style={styles.textoBotao}>Somar</Text>
+			</TouchableOpacity>  
+		</View>
     </View>
+	
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bloco:{
+	  marginTop:20,
+	  width:'80%',
+	  marginLeft:'10%'
+  },
+  titulo:{
+	  fontSize:30,
+	  textAlign:'center'
+  },
+  input:{
+	borderWidth:2,
+	fontSize:20,
+	borderRadius:10
+  },
+  botao:{
+	  backgroundColor:'#000',
+	  borderRadius:10
+  },
+  textoBotao:{
+	  color:'#FFF',
+	  textAlign:'center',
+	  fontSize:20,
+  }
 });
